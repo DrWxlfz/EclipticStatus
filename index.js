fa38c9e8882bdaaf0d49cec51634099bc6b2e5b3
MrWxlfz (Luke) <157859094+DrWxlfz@users.noreply.github.com>
2024-11-16 13:34:28 -0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["❄️ It's cold outside!", "eclipticlabs.weblflow.io"];
+const statusMessages = ["eclipticlabs.xyz", "?help"];
 const statusTypes = [ 'idle', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
